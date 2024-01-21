77fd05542623b63eae54eb4498c8d72f3d135d2e
P0t1on <ini4343@naver.com>
2024-01-21 14:12:18 +0000

    h

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-    host: '0.0.0.0', // Change this to a valid IP address if needed
-    port: 4000, // Optional otherwise your app will start on default port
-  },
+	plugins: [sveltekit()]
 });
