ee28bfa6f9b1be3a0fd625df9d5a753abdcda398
P0t1on <ini4343@naver.com>
2024-01-26 12:56:08 +0000

    types 통합

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -19,5 +19,63 @@ export const Vector2 = {
   }
 } as const;
 
+export type Entity = {
+  name: string;
+  static: boolean;
+  sprite: string | ImageBitmap;
+  module: any;
+  config: {
+    moveable: boolean;
+  };
+};
 
-const test: Vector2 = {x: 1, y: 2}
+// build shape
+
+type BuildShape = {
+  rect: {
+    shape: "rect";
+    position: Vector3;
+    size: Vector2;
+  };
+  line: {
+    shape: "line";
+    layer: number;
+    startPosition: Vector2;
+    endPosition: Vector2;
+  };
+};
+
+// build method
+
+type BuildMethod = {
+  fill: {
+    method: "fill";
+    entity: string;
+  };
+};
+
+export type buildScriptType = BuildMethod["fill"] &
+  (BuildShape["line"] | BuildShape["rect"]);
+
+export type MapFormat = {
+  name: string;
+  /* URL domain */ domain: string;
+  layer: number;
+  width: number;
+  height: number;
+  baseTile: string;
+  sprites: {
+    name: string;
+    /* URL */ src: string;
+  }[];
+  entityDef: {
+    name: string;
+    sprite: string;
+    /* URL */ module: string;
+    config?: {
+      moveable?: boolean;
+    };
+  }[];
+  buildScripts: buildScriptType[];
+  startPoint: Vector3;
+};
