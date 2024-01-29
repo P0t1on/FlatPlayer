0c50e95542cdfd2a1285bbd961afdff432e499be
P0t1on <ini4343@naver.com>
2024-01-29 05:32:34 +0000

    월드 데이터 업뎃

--- static/FlatMaps/ambient.d.ts
+++ static/FlatMaps/ambient.d.ts
@@ -1,4 +1,24 @@
-export {}
-declare global {
-    export function setEntity(entityName: string, x: number, y: number, z: number): void
-}
+declare function setEntity(
+  entityName: string,
+  x: number,
+  y: number,
+  z: number
+): void;
+
+declare const world: {
+  width: number;
+  height: number;
+};
+
+declare type EntityConfig = {
+  NONE: 0b00;
+  STATIC: 0b01;
+  MOVEABLE: 0b10;
+};
+
+declare const entity: {
+  name: string;
+  sprite: string;
+  module: boolean;
+  config: number;
+};
--- static/FlatMaps/scripts/Wall.ts
+++ static/FlatMaps/scripts/Wall.ts
@@ -1,2 +1,1 @@
-
-console.log(entity)
+// console.log(entity)
