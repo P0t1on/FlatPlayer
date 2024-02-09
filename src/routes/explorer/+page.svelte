fb97ebbc7effaec64890913e163a6d92fd5da971
P0t1on <ini4343@naver.com>
2024-02-09 14:52:29 +0000

    컨텐츠 탐색 페이지 구현

---
+++ src/routes/explorer/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+</script>
+
+<section>
+    explorer page
+</section>
+
+<style lang="scss">
+    
+</style>
