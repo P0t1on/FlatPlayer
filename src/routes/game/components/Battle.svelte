deef5e42257ad39067208e358fe0596fb61968eb
Potion <ini4343@naver.com>
2024-02-16 14:54:02 +0000

    전투 씬 구상

--- src/routes/game/components/Battle.svelte
+++ src/routes/game/components/Battle.svelte
@@ -1,19 +1,33 @@
 <script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+
   export let width = 100,
     height = 100;
+
+  const dispatch = createEventDispatcher();
 </script>
 
 <article id="battle" style={`width:${width}px; height:${height}px`}>
-  text
+  <div id="health"></div>
+  <div id="field"></div>
+  <div id="actions"></div>
+  <div id="etc">
+    <div id="specials"></div>
+    <div id="consumes"></div>
+  </div>
 </article>
 
 <style lang="scss">
   article#battle {
-    z-index: 2;
+    display: flex;
     position: absolute;
+
+    z-index: 2;
+    padding: 8px;
+    line-height: 10px;
+
     color: white;
     background-color: black;
-
     border: 2px solid gray;
   }
 </style>
