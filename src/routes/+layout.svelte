11b9f62205ed7483976496767af7341e55e16d85
Potion <ini4343@naver.com>
2024-03-13 13:38:46 +0000

    google material icon 정의 위치 변경

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,6 +14,10 @@
 </script>
 
 <svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+  />
   <script
     src="https://raw.githubusercontent.com/LeaVerou/prefixfree/gh-pages/prefixfree.min.js"
   ></script>
--- src/routes/documents/Explorer.svelte
+++ src/routes/documents/Explorer.svelte
@@ -21,13 +21,6 @@
   });
 </script>
 
-<svelte:head>
-  <link
-    rel="stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-  />
-</svelte:head>
-
 <article id="explorer">
   <div id="search">
     <input
