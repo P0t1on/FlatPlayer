0abe629d8e3fdb1680fb82e808f98c7653885da8
Potion <ini4343@naver.com>
2024-04-08 14:55:41 +0000

    신규 액티비티 Basement 구현중

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -1,14 +1,25 @@
 <script lang="ts">
-  import './+style.scss';
+  import { onMount, type ComponentType } from 'svelte';
+  import Adventure from './activities/adventure/Adventure.svelte';
+  import Basement from './activities/basement/Basement.svelte';
 
+  import './style.scss';
+
+  const comps = {
+    basement: Basement,
+    adventure: Adventure,
+  };
   let gameName = '';
+  let component: keyof typeof comps = 'basement';
+
+  onMount(() => {});
 </script>
 
 <svelte:head>
-  <title>{gameName}</title>
+  <title>{gameName} - {component}</title>
 </svelte:head>
 
-<slot />
+<svelte:component this={comps[component]} />
 
 <style lang="scss">
 </style>
---
+++ src/routes/game/activities/basement/Basement.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import './style.scss';
+
+  let items: {
+    [key: string]: {
+      value: number;
+      max: number | false;
+    };
+  } = {};
+
+  let actions: {
+    id: string;
+    cooltime: number | false;
+    method: () => void;
+    currentCooltime: number;
+  }[] = [];
+</script>
+
+<section id="basement"></section>
+
+<style lang="scss">
+  section#basement {
+    height: 100%;
+    display: flex;
+  }
+</style>
