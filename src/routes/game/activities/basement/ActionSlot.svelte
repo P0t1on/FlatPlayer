6420bcf97d4aeae1efbecf48b32920aab0a1f5f9
Potion <ini4343@naver.com>
2024-04-10 14:51:53 +0000

    basement 구현중

---
+++ src/routes/game/activities/basement/ActionSlot.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let name: string, cooltime: number | false, method: () => void;
+</script>
+
+<li class="actionSlot"></li>
+
+<style lang="scss">
+  li.actionSlot {
+    background-color: white;
+  }
+</style>
--- src/routes/game/activities/basement/Basement.svelte
+++ src/routes/game/activities/basement/Basement.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import ActionSlot from './ActionSlot.svelte';
   import './style.scss';
 
   let items: {
@@ -10,17 +11,40 @@
 
   let actions: {
     id: string;
+    name: string;
     cooltime: number | false;
     method: () => void;
     currentCooltime: number;
   }[] = [];
 </script>
 
-<section id="basement"></section>
+<section id="basement">
+  <ul id="actionList">
+    {#each actions as { currentCooltime, id, ...args }, i (i)}
+      <ActionSlot {...args} />
+    {/each}
+  </ul>
+</section>
 
 <style lang="scss">
   section#basement {
     height: 100%;
     display: flex;
+
+    ul#actionList {
+      border: 2px solid white;
+
+      overflow-y: auto;
+
+      &::-webkit-scrollbar {
+        width: 8px;
+        background-color: transparent;
+        outline: solid 2px white;
+      }
+
+      &::-webkit-scrollbar-thumb {
+        background-color: white;
+      }
+    }
   }
 </style>
