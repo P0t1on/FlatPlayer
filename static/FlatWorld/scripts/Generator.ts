8590937e8d08170e0870c7719dc477f46bf6c84d
DevPotion <156809181+P0t1on@users.noreply.github.com>
2024-04-22 13:34:44 +0000

    맵 데이터 포맷 갈아엎기

--- static/FlatWorld/scripts/Generator.ts
+++
@@ -1,8 +0,0 @@
-// setEntity("wall", 1, 1, 0)
-//console.log(world);
-
-for (let y = 1; y < world.height + 1; y++) {
-  for (let x = 1; x < world.width + 1; x++) {
-    if(x === 1 || y === 1 || x === world.width || y === world.height) setEntity("wall", x, y, 0);
-  }
-}
