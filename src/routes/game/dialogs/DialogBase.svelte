346644faf29ae1e5675840e16c988d006e29f8f8
DevPotion <156809181+P0t1on@users.noreply.github.com>
2024-05-04 13:17:42 +0000

    다이얼로그 베이스 컴포넌트  구현

--- src/routes/game/dialogs/DialogBase.svelte
+++ src/routes/game/dialogs/DialogBase.svelte
@@ -1,14 +1,6 @@
 <script lang="ts">
+  import { SvgIcon } from '$lib/Util';
   import { createEventDispatcher } from 'svelte';
-  import { icon } from '$lib/Util';
-
-  export let dialogID: string;
-
-  let main: HTMLElement,
-    closer: HTMLImageElement,
-    isDrag = false,
-    x = 0,
-    y = 0;
 
   const dispatch = createEventDispatcher<{
     focus: void;
@@ -16,65 +8,146 @@
     submit: [() => void];
   }>();
 
-  const down = (e: MouseEvent) => {
-      if (e.target !== closer) {
-        dispatch('focus');
-        isDrag = true;
-        x = e.clientX;
-        y = e.clientY;
-      }
-    },
-    move = (e: MouseEvent) => {
-      if (isDrag) {
-        const dx = e.clientX - x;
-        const dy = e.clientY - y;
+  export let title: string, description: string, zIndex: number;
 
-        main.style.top = `${main.offsetTop + dy}px`;
-        main.style.left = `${main.offsetLeft + dx}px`;
+  let main: HTMLElement,
+    closer: HTMLSpanElement,
+    isDrag = false,
+    x = 0,
+    y = 0;
 
-        x = e.clientX;
-        y = e.clientY;
-      }
-    },
-    up = (e: MouseEvent) => {
-      isDrag = false;
-    };
+  function down(e: MouseEvent) {
+    if (e.target !== closer && !closer.contains(e.target as HTMLElement)) {
+      dispatch('focus');
+      isDrag = true;
+      x = e.clientX;
+      y = e.clientY;
+    }
+  }
+
+  function move(e: MouseEvent) {
+    if (isDrag) {
+      const dx = e.clientX - x;
+      const dy = e.clientY - y;
+
+      main.style.top = `${main.offsetTop + dy}px`;
+      main.style.left = `${main.offsetLeft + dx}px`;
+
+      x = e.clientX;
+      y = e.clientY;
+    }
+  }
+
+  function up(e: MouseEvent) {
+    isDrag = false;
+  }
 </script>
 
 <svelte:document on:mousemove={move} on:mouseup={up} />
 
-<article id={dialogID} class:dialogBase={dialogID ? false : true}>
+<article id="dialog" bind:this={main}>
   <slot name="tab">
     <div class="tab" on:mousedown={down} role="presentation">
-      <img
-        class="nodrag"
+      <span>{title}</span>
+      <span
+        class="nodrag button"
         bind:this={closer}
-        alt="close"
-        src={icon('close')}
         on:click={() => dispatch('destroy')}
         role="presentation"
-      />
+      >
+        <SvgIcon type="close" color="white" />
+      </span>
     </div>
   </slot>
-  <slot name="description">
-    <div class="desc">sample text</div>
-  </slot>
-  <slot name="interaction">
+  <slot name="content">
+    <div class="desc">{description}</div>
+
     <div class="submit">
-      <img
+      <span
         class="nodrag"
-        alt="submit"
-        src={icon('done')}
         on:click={() => {
           let submitDelete = true;
           dispatch('submit', [() => (submitDelete = false)]);
           if (submitDelete) dispatch('destroy');
         }}
         role="presentation"
-      />
+      >
+        <SvgIcon type="done" color="white" />
+      </span>
     </div>
   </slot>
 </article>
 
 <style lang="scss" module>
+  article#dialog {
+    position: absolute;
+    background-color: black;
+    color: white;
+
+    top: 30%;
+    left: 40%;
+    min-width: 100px;
+    z-index: bind(zIndex);
+
+    border-radius: 8px;
+    outline: 4px double white;
+
+    user-select: none;
+
+    div.tab {
+      border-radius: 8px 8px 0 0;
+      padding: 4px 8px 4px 8px;
+      min-width: 12vw;
+      background-color: black;
+      display: flex;
+      justify-content: space-between;
+      outline: 4px double white;
+
+      span.button {
+        height: 24px;
+        cursor: pointer;
+        transition: all ease 0.5s;
+        border-radius: 4px;
+
+        &:hover {
+          box-shadow: 0 0 1px 1px gray;
+          color: white;
+        }
+
+        &:active {
+          filter: invert(100%);
+          box-shadow: none;
+          background-color: gray;
+        }
+      }
+    }
+
+    div.desc {
+      padding: 6px;
+    }
+
+    div.submit {
+      display: flex;
+      justify-content: flex-end;
+      padding: 6px;
+
+      span {
+        height: 24px;
+        border-radius: 4px;
+        cursor: pointer;
+        transition: all ease 0.5s;
+
+        &:hover {
+          box-shadow: 0 0 3px 3px gray;
+          color: white;
+        }
+
+        &:active {
+          filter: invert(100%);
+          box-shadow: none;
+          background-color: gray;
+        }
+      }
+    }
+  }
 </style>
