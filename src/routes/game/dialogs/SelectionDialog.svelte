0a26d6f08aa2cd0b45d59a65a627ddfd07f07ccc
DevPotion <156809181+P0t1on@users.noreply.github.com>
2024-06-11 14:38:18 +0000

    텍스트 재생 애니메이션 적용

--- src/routes/game/dialogs/SelectionDialog.svelte
+++ src/routes/game/dialogs/SelectionDialog.svelte
@@ -7,9 +7,15 @@
     description: string,
     zIndex: number,
     menu: string[],
-    canIgnore: boolean;
+    canIgnore: boolean,
+    textSpeed = 25;
 
-  let renderMenus: [string, string | undefined][];
+  let renderMenus: [string, string | undefined][],
+    descAnim: NodeJS.Timeout,
+    isDescAnimPlaying = true,
+    readySelection = false;
+
+  let descDiv: HTMLDivElement;
 
   const dispatch = createEventDispatcher<{
     focus: void;
@@ -19,6 +25,8 @@
 
   function submit(index: number) {
     return function () {
+      if (isDescAnimPlaying) return;
+
       let preventDefault = false;
       dispatch('submit', [() => (preventDefault = true), index]);
 
@@ -26,11 +34,43 @@
     };
   }
 
+  function skipDescAnim() {
+    isDescAnimPlaying = false;
+    if (descAnim) clearInterval(descAnim);
+    descDiv.innerText = description;
+    readySelection = true;
+  }
+
+  function onKeyDown(
+    e: KeyboardEvent & {
+      currentTarget: EventTarget & Document;
+    }
+  ) {
+    if (e.keyCode === 32) skipDescAnim();
+  }
+
   onMount(() => {
+    {
+      descDiv.innerText = description;
+      descDiv.style.width = descDiv.offsetWidth + 'px';
+      descDiv.style.height = descDiv.offsetHeight + 'px';
+      descDiv.innerText = '';
+    }
+
+    let i = 0,
+      m = description.length;
+    descAnim = setInterval(() => {
+      if (!isDescAnimPlaying) return;
+      descDiv.innerText += description[i];
+      i++;
+      if (i >= m) {
+        skipDescAnim();
+      }
+    }, textSpeed);
+
     const even = menu.length % 2 === 0,
       length = Math.ceil(menu.length / 2);
     renderMenus = Array.from({ length }, (_, i) => {
-      console.log(i);
       return [
         menu[i * 2],
         i !== length - 1 || even ? menu[i * 2 + 1] : undefined,
@@ -39,24 +79,35 @@
   });
 </script>
 
+<svelte:document on:keydown={onKeyDown} />
+
 <DialogBase
   {...{ title, zIndex, canIgnore, overrideContent: true }}
   on:focus={() => dispatch('focus')}
   on:destroy={() => dispatch('destroy')}
 >
-  <div slot="content">
-    <div class="desc">{description}</div>
+  <div
+    slot="content"
+    on:click={skipDescAnim}
+    role="presentation"
+    style={isDescAnimPlaying ? 'cursor: pointer;' : 'cursor: auto;'}
+  >
+    <div class="desc" bind:this={descDiv} />
     {#if renderMenus !== undefined}
       <div class="selection">
         <div>
           {#each renderMenus as [a], i}
-            <button on:click={submit(i * 2)}>{a}</button>
+            <button class:ready={readySelection} on:click={submit(i * 2)}>
+              {a}
+            </button>
           {/each}
         </div>
         <div>
           {#each renderMenus as [_, b], i}
             {#if b !== undefined}
-              <button on:click={submit(i * 2 + 1)}>{b}</button>
+              <button class:ready={readySelection} on:click={submit(i * 2 + 1)}>
+                {b}
+              </button>
             {/if}
           {/each}
         </div>
@@ -68,6 +119,8 @@
 <style lang="scss" module>
   div.desc {
     padding: 6px;
+    word-wrap: break-word;
+    white-space: break-spaces;
   }
 
   div.selection {
@@ -81,7 +134,11 @@
       width: 100%;
 
       button {
+        cursor: pointer;
         text-align: center;
+        transition: all ease 0.5s;
+
+        opacity: 30%;
         color: white;
         background-color: transparent;
         border: 1px solid white;
@@ -90,15 +147,18 @@
         margin: 4px 0 4px 0;
         width: 100%;
 
-        &:hover {
-          transition: all ease 0.5s;
-          box-shadow: white 0 0 8px 2px;
-          cursor: pointer;
+        &.ready {
+          opacity: 100%;
 
-          &:active {
-            box-shadow: none;
-            background-color: white;
-            color: black;
+          &:hover {
+            box-shadow: white 0 0 8px 2px;
+            cursor: pointer;
+
+            &:active {
+              box-shadow: none;
+              background-color: white;
+              color: black;
+            }
           }
         }
       }
