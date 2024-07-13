51814cb7f24461e77cb7b3abd5b4d967147c938a
DevPotion <156809181+P0t1on@users.noreply.github.com>
2024-07-13 03:58:14 +0000

    애니메이션 구현 및 코드 최적화

--- src/routes/game/dialogs/BattleDialog/EntitySlot.svelte
+++ src/routes/game/dialogs/BattleDialog/EntitySlot.svelte
@@ -6,16 +6,18 @@
   export let entity: EntityInstanceType | undefined = undefined;
 
   let hpSub: () => void,
-    hpVal = 0,
+    hpVal = '0%',
     hpTitle = '',
     spSub: () => void,
-    spVal = 0,
+    spVal = '0%',
     spTitle = '';
 
   $: {
+    // 이전에 구독했던 이벤트 갱신(구독해제)
     hpSub?.();
     spSub?.();
 
+    // 프로그래스 바 연동
     if (entity !== undefined) {
       const {
         hp: { current: hp, max: mhp },
@@ -24,13 +26,13 @@
 
       hpSub = hp.subscribe((v) => {
         const max = get(mhp);
-        hpVal = (100 * v) / max;
-        hpTitle = `💖 ${v} / ${max}`;
+        hpVal = (100 * v) / max + '%';
+        hpTitle = isNaN(v) ? '💖 ? / ? ' : `💖 ${v}p / ${max}p`;
       });
       spSub = sp.subscribe((v) => {
         const max = get(msp);
-        spVal = (100 * v) / max;
-        spTitle = `✨ ${v} / ${max}`;
+        spVal = (100 * v) / max + '%';
+        spTitle = isNaN(v) ? '✨ ? / ? ' : `✨ ${v}p / ${max}p`;
       });
     }
   }
@@ -42,40 +44,12 @@
       {entity.name}
     </span>
     <div class="status">
-      {#if isNaN(hpVal)}
-        <progress
-          class="hp infinite"
-          title="💖 ? / ?"
-          use:statTooltip
-          max={100}
-          value={100}
-        />
-      {:else}
-        <progress
-          class="hp"
-          title={hpTitle}
-          use:statTooltip
-          max={100}
-          value={hpVal}
-        />
-      {/if}
-      {#if isNaN(spVal)}
-        <progress
-          class="sp infinite"
-          title="✨ ? / ?"
-          use:statTooltip
-          max={100}
-          value={100}
-        />
-      {:else}
-        <progress
-          class="sp"
-          title={spTitle}
-          use:statTooltip
-          max={100}
-          value={spVal}
-        />
-      {/if}
+      <div class="progress" title={hpTitle} use:statTooltip>
+        <div class="bar hp">&nbsp;</div>
+      </div>
+      <div class="progress" title={spTitle} use:statTooltip>
+        <div class="bar sp">&nbsp;</div>
+      </div>
     </div>
   {/if}
 </div>
@@ -86,6 +60,39 @@
 
     div.status {
       width: min-content;
+
+      div.progress {
+        min-width: 150px;
+        width: 100%;
+        margin: 4px;
+        padding: 0;
+        background-color: rgba(128, 128, 128, 0.4);
+        border-radius: 12px;
+        overflow: hidden;
+
+        div.bar {
+          font-size: smaller;
+          height: min-content;
+          &.sp {
+            background: linear-gradient(
+              90deg,
+              rgba(97, 73, 154, 0.8) 25%,
+              rgba(255, 255, 255, 0.4) 50%,
+              rgba(97, 73, 154, 0.8) 75%
+            );
+            width: bind(spVal);
+
+            background-size: 200% 100%;
+            animation: shimmer 2s infinite;
+            transition: width 0.3s;
+          }
+
+          &.hp {
+            background: crimson;
+            width: bind(hpVal);
+          }
+        }
+      }
 
       progress {
         appearance: none;
@@ -105,6 +112,15 @@
           background-color: #61499a;
         }
       }
+    }
+  }
+
+  @keyframes shimmer {
+    0% {
+      background-position: -200% 0;
+    }
+    100% {
+      background-position: 200% 0;
     }
   }
 </style>
