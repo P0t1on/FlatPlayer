6cc1a80f22456afd4c8033a19e62c34ce75573a0
DevPotion <156809181+P0t1on@users.noreply.github.com>
2024-07-26 14:56:22 +0000

    웹용 이벤트에미터 구현

--- src/lib/Util.ts
+++ src/lib/Util.ts
@@ -59,3 +59,66 @@ export function getUid() {
 
   return uids.shift() as number;
 }
+
+export class EventManager<M extends { [key: number | string | symbol]: any }> {
+  private listener: { [K in keyof M]: Set<(arg: M[K]) => void> } =
+    {} as typeof this.listener;
+
+  public constructor() {}
+
+  public emit<K extends keyof M>(key: K, args: M[K]): void {
+    const listenerList = this.listener[key];
+
+    if (listenerList === undefined) return;
+
+    for (const listener of listenerList) {
+      listener(args);
+    }
+  }
+
+  public async emitThen<K extends keyof M>(key: K, args: M[K]) {
+    const handlers = this.listener[key];
+
+    if (handlers === undefined) return;
+
+    for await (const handler of handlers) {
+      handler(args);
+    }
+
+    return;
+  }
+
+  public on<K extends keyof M>(key: K, handler: (arg: M[K]) => void) {
+    let handlers = this.listener[key];
+
+    if (handlers === undefined) {
+      handlers = this.listener[key] = new Set();
+    }
+
+    handlers.add(handler);
+
+    return () => handlers.delete(handler);
+  }
+
+  public once<K extends keyof M>(key: K, handler: (arg: M[K]) => void) {
+    let handlers = this.listener[key];
+
+    if (handlers === undefined) {
+      handlers = this.listener[key] = new Set();
+    }
+
+    const wrapper: typeof handler = (v) => {
+      handler(v);
+      handlers.delete(wrapper);
+    };
+
+    handlers.add(wrapper);
+
+    return () => handlers.delete(wrapper);
+  }
+}
+
+const test = new EventManager<{
+  321: number;
+  513: string;
+}>();
